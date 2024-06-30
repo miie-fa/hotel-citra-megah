d74874b1788662b45e877230b2b5652d6e76c968
Farras <115802691+miie-fa@users.noreply.github.com>
2024-06-30 17:10:02 +0700

    payment update

--- app/Http/Controllers/Front/BookingController.php
+++ app/Http/Controllers/Front/BookingController.php
@@ -170,9 +170,6 @@ class BookingController extends Controller
         $request->validate([
             'billing_name' => 'required',
             'billing_email' => 'required|email',
-            'billing_phone' => 'required',
-            'billing_country' => 'required',
-            'billing_address' => 'required',
         ]);
 
         session()->put('billing_name',$request->billing_name);
@@ -186,164 +183,119 @@ class BookingController extends Controller
     }
 
     public function pay_at_hotel(Request $request,$final_price)
+{
+    $order_no = time();
+
+    $order = Order::create([
+        'user_id' => Auth::user()->id,
+        'order_no' => $order_no,
+        'payment_method' => 'Pay At Hotel',
+        'paid_amount' => $final_price,
+        'booking_date' => date('d/m/Y'),
+        'status' => 'SUCCESS',
+    ]);
+
+    $orders_id = $order->id;
+
+    $arr_cart_room_id = array();
+    $i=0;
+    foreach(session()->get('cart_room_id') as $value) {
+        $arr_cart_room_id[$i] = $value;
+        $i++;
+    }
+
+    $arr_cart_checkin_date = array();
+    $i=0;
+    foreach(session()->get('cart_checkin_date') as $value) {
+        $arr_cart_checkin_date[$i] = $value;
+        $i++;
+    }
+
+    $arr_cart_checkout_date = array();
+    $i=0;
+    foreach(session()->get('cart_checkout_date') as $value) {
+        $arr_cart_checkout_date[$i] = $value;
+        $i++;
+    }
+
+    $arr_cart_adult = array();
+    $i=0;
+    foreach(session()->get('cart_adult') as $value) {
+        $arr_cart_adult[$i] = $value;
+        $i++;
+    }
+
+    $arr_cart_children = array();
+    $i=0;
+    foreach(session()->get('cart_children') as $value) {
+        $arr_cart_children[$i] = $value;
+        $i++;
+    }
+
+    for($i=0;$i<count($arr_cart_room_id);$i++)
     {
-        $order_no = time();
+        $r_info = Room::where('id',$arr_cart_room_id[$i])->first();
+        $d1 = explode('/',$arr_cart_checkin_date[$i]);
+        $d2 = explode('/',$arr_cart_checkout_date[$i]);
+        $d1_new = $d1[2].'-'.$d1[1].'-'.$d1[0];
+        $d2_new = $d2[2].'-'.$d2[1].'-'.$d2[0];
+        $t1 = strtotime($d1_new);
+        $t2 = strtotime($d2_new);
+        $diff = ($t2-$t1)/60/60/24;
+        $sub = $r_info->price*$diff;
 
-        $order = Order::create([
-            'user_id' => Auth::user()->id,
-            'order_no' => $order_no,
-            'payment_method' => 'Pay At Hotel',
-            'paid_amount' => $final_price,
-            'booking_date' => date('d/m/Y'),
-            'status' => 'SUCCESS',
-        ]);
+        $obj = new OrderDetail();
+        $obj->order_id = $orders_id;
+        $obj->room_id = $arr_cart_room_id[$i];
+        $obj->room_name = $r_info->name;
+        $obj->checkin_date = $arr_cart_checkin_date[$i];
+        $obj->checkout_date = $arr_cart_checkout_date[$i];
+        $obj->night = $diff;
+        $obj->adult = $arr_cart_adult[$i];
+        $obj->children = $arr_cart_children[$i];
+        $obj->subtotal = $sub;
+        $obj->save();
 
-        $orders_id = $order->id;
-
-        $arr_cart_room_id = array();
-        $i=0;
-        foreach(session()->get('cart_room_id') as $value) {
-            $arr_cart_room_id[$i] = $value;
-            $i++;
-        }
-
-        $arr_cart_checkin_date = array();
-        $i=0;
-        foreach(session()->get('cart_checkin_date') as $value) {
-            $arr_cart_checkin_date[$i] = $value;
-            $i++;
-        }
-
-        $arr_cart_checkout_date = array();
-        $i=0;
-        foreach(session()->get('cart_checkout_date') as $value) {
-            $arr_cart_checkout_date[$i] = $value;
-            $i++;
-        }
-
-        $arr_cart_adult = array();
-        $i=0;
-        foreach(session()->get('cart_adult') as $value) {
-            $arr_cart_adult[$i] = $value;
-            $i++;
-        }
-
-        $arr_cart_children = array();
-        $i=0;
-        foreach(session()->get('cart_children') as $value) {
-            $arr_cart_children[$i] = $value;
-            $i++;
-        }
-
-        for($i=0;$i<count($arr_cart_room_id);$i++)
-        {
-            $r_info = Room::where('id',$arr_cart_room_id[$i])->first();
-            $d1 = explode('/',$arr_cart_checkin_date[$i]);
-            $d2 = explode('/',$arr_cart_checkout_date[$i]);
-            $d1_new = $d1[2].'-'.$d1[1].'-'.$d1[0];
-            $d2_new = $d2[2].'-'.$d2[1].'-'.$d2[0];
-            $t1 = strtotime($d1_new);
-            $t2 = strtotime($d2_new);
-            $diff = ($t2-$t1)/60/60/24;
-            $sub = $r_info->price*$diff;
-
-            $obj = new OrderDetail();
-            $obj->order_id = $orders_id;
-            $obj->room_id = $arr_cart_room_id[$i];
-            $obj->room_name = $r_info->name;
-            $obj->checkin_date = $arr_cart_checkin_date[$i];
-            $obj->checkout_date = $arr_cart_checkout_date[$i];
-            $obj->night = $diff;
-            $obj->adult = $arr_cart_adult[$i];
-            $obj->children = $arr_cart_children[$i];
-            $obj->subtotal = $sub;
-            $obj->save();
-
-            while(1) {
-                if($t1>=$t2) {
-                    break;
-                }
-
-                $obj = new BookedRoom();
-                $obj->booking_date = date('d/m/Y',$t1);
-                $obj->order_no = $order_no;
-                $obj->room_id = $arr_cart_room_id[$i];
-                $obj->save();
-
-                $t1 = strtotime('+1 day',$t1);
+        while(1) {
+            if($t1>=$t2) {
+                break;
             }
 
+            $obj = new BookedRoom();
+            $obj->booking_date = date('d/m/Y',$t1);
+            $obj->order_no = $order_no;
+            $obj->room_id = $arr_cart_room_id[$i];
+            $obj->save();
+
+            $t1 = strtotime('+1 day',$t1);
         }
-
-        $subject = 'Pemesanan Kamar Hotel Berhasil';
-        $message = '<html><body>';
-        $message .= '<h1 style="color: #333;">You have made an order for hotel booking.</h1>';
-        $message .= '<p style="color: #666;">The booking information is given below:</p>';
-        $message .= '<ul>';
-        $message .= '<li><strong>Order No:</strong> '.$order_no.'</li>';
-        $message .= '<li><strong>Payment Method:</strong> Pay At Hotel</li>';
-        $message .= '<li><strong>Paid Amount:</strong> '.number_format($final_price, 2, ',', '.').'</li>';
-        $message .= '<li><strong>Booking Date:</strong> '.date('d/m/Y').'</li>';
-        $message .= '</ul>';
-        $message .= '</body></html>';
-
-        for($i=0;$i<count($arr_cart_room_id);$i++) {
-
-            $r_info = Room::where('id',$arr_cart_room_id[$i])->first();
-
-            $message .= '<br>Room Name: '.$r_info->name;
-            $message .= '<br>Price Per Night: $'.$r_info->price;
-            $message .= '<br>Checkin Date: '.$arr_cart_checkin_date[$i];
-            $message .= '<br>Checkout Date: '.$arr_cart_checkout_date[$i];
-            $message .= '<br>Adult: '.$arr_cart_adult[$i];
-            $message .= '<br>Children: '.$arr_cart_children[$i].'<br>';
-        }
-
-        $customer_name = Auth::user()->name;
-
-        $customer_email = Auth::user()->email;
-
-        $customer_phone = Auth::user()->phone;
-
-        Mail::to($customer_email)->send(new HelloMail($subject,$message));
-
-        $messages = "Selamat kak $customer_name anda berhasil melakukan pemesanan!";
-
-        try {
-            $this->send_message($customer_phone, $messages);
-        } catch (\Exception $e) {
-            dd("Error sending message: " . $e->getMessage());
-        }
-
-        // dd($customer_phone);
-
-
-        session()->forget('cart_room_id');
-        session()->forget('cart_checkin_date');
-        session()->forget('cart_checkout_date');
-        session()->forget('cart_adult');
-        session()->forget('cart_children');
-        session()->forget('billing_name');
-        session()->forget('billing_email');
-        session()->forget('billing_phone');
-        session()->forget('billing_country');
-        session()->forget('billing_address');
-        session()->forget('billing_notes');
-
-        // if ($user) {
-        //     $message = "$notify->payment_notify";
-        //     $this->send_message($user->phone, $message);
-
-        //     $messages = "Selamat anda berhasil Daftar ini kode OTP anda Jangan beri ini kepada Siapapun $user->token";
-
-        //     $this->send_message($user->phone,$messages);
-        // } else {
-        //     return response()->json(['error' => 'User not found'], 404);
-        // }
-
-
-        return redirect()->route('home')->with('success', 'Payment is successful');
     }
+
+    $customer_name = Auth::user()->name;
+    $customer_phone = Auth::user()->phone;
+
+    $messages = "Selamat kak $customer_name anda berhasil melakukan pemesanan!";
+
+    try {
+        $this->send_message($customer_phone, $messages);
+    } catch (\Exception $e) {
+        dd("Error sending message: " . $e->getMessage());
+    }
+
+    session()->forget('cart_room_id');
+    session()->forget('cart_checkin_date');
+    session()->forget('cart_checkout_date');
+    session()->forget('cart_adult');
+    session()->forget('cart_children');
+    session()->forget('billing_name');
+    session()->forget('billing_email');
+    session()->forget('billing_phone');
+    session()->forget('billing_country');
+    session()->forget('billing_address');
+    session()->forget('billing_notes');
+
+    return redirect()->route('home')->with('success', 'Payment is successful');
+}
 
     public $va;
     public $apiKey;
